3543ffd8e875747060c6065c3354c45cbd4dfc47
MAIN\ltr6702 <loganr365@yahoo.com>
2017-04-14 16:31:42 -0400

    Logan, phil cam push

---
+++ LoganPhil_CAM/Camera.cpp
@@ -0,0 +1,21 @@
+/*#include <iostream>
+#include <windows.h>
+#include "Camera.h"
+#include "RE\ReEngAppClass.h"
+#include <SFML\Graphics.hpp>
+using namespace std;
+
+/*int main(void)
+{
+	Camera* oFoo = Camera::GetInstance();
+	cout << oFoo->m_nVar << endl;
+
+
+	Camera::ReleaseInstance();
+
+	Camera* oBar = Camera::GetInstance();
+	oBar->m_nVar = 50;
+	cout << oFoo->m_nVar << endl;
+
+	getchar();
+}*/
