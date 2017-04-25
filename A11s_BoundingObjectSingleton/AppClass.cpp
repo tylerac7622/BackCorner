fd73b43c5acedbe1ed467164735a2da195b19c0f
tylerac7622 <tac7622@ad.rit.edu>
2017-04-25 18:24:33 -0400

    For A11, The planes properly generate around the objects

--- A11s_BoundingObjectSingleton/AppClass.cpp
+++ A11s_BoundingObjectSingleton/AppClass.cpp
@@ -17,15 +17,20 @@ void AppClass::InitVariables(void)
 	m_v3O1 = vector3(-2.5f, 0.0f, 0.0f);
 	m_v3O2 = vector3(2.5f, 0.0f, 0.0f);
 
+	m_pPlane = new PrimitiveClass();
+	m_pPlane->GeneratePlane(5.0f, REYELLOW);
+
 	//Load Models
 	m_pMeshMngr->LoadModel("Minecraft\\Steve.obj", "Steve");
 	m_pMeshMngr->LoadModel("Minecraft\\Creeper.obj", "Creeper");
-	m_pMeshMngr->LoadModel("Minecraft\\Cow.obj", "Cow");
 
 	m_pBOMngr = MyBOManager::GetInstance();
 	m_pBOMngr->AddObject(m_pMeshMngr->GetVertexList("Steve"), "Steve");
 	m_pBOMngr->AddObject(m_pMeshMngr->GetVertexList("Creeper"), "Creeper");
-	m_pBOMngr->AddObject(m_pMeshMngr->GetVertexList("Cow"), "Cow");
+}
+
+void CheckSAT(void)
+{
 }
 
 void AppClass::Update(void)
@@ -48,21 +53,14 @@ void AppClass::Update(void)
 	static int nClock = m_pSystem->GenClock();
 	float fDeltaTime = static_cast<float>(m_pSystem->LapClock(nClock));
 	fTimer += fDeltaTime;
-	static vector3 v3Start = vector3(2.5f, 0.0f, 0.0f);
-	static vector3 v3End = vector3(5.0, 0.0, 0.0);
-	float fPercentage = MapValue(fTimer, 0.0f, 3.0f, 0.0f, 1.0f);
-	m_v3O2 = glm::lerp(v3Start, v3End, fPercentage);
-	matrix4 mTranslation = glm::translate(m_v3O2);
 
 	//Set the model matrices for both objects and Bounding Spheres
 	m_pMeshMngr->SetModelMatrix(glm::translate(m_v3O1) * ToMatrix4(m_qArcBall), "Steve");
 	m_pMeshMngr->SetModelMatrix(glm::translate(m_v3O2), "Creeper");
-	m_pMeshMngr->SetModelMatrix(glm::translate(m_v3O3), "Cow");
 
 	//Set the model matrix to the Bounding Object
 	m_pBOMngr->SetModelMatrix(m_pMeshMngr->GetModelMatrix("Steve"), "Steve");
 	m_pBOMngr->SetModelMatrix(m_pMeshMngr->GetModelMatrix("Creeper"), "Creeper");
-	m_pBOMngr->SetModelMatrix(m_pMeshMngr->GetModelMatrix("Cow"), "Cow");
 
 	m_pBOMngr->Update();//Update collision detection
 	
@@ -73,11 +71,7 @@ void AppClass::Update(void)
 	//Adds all loaded instance to the render list
 	m_pMeshMngr->AddInstanceToRenderList("ALL");
 
-	if (fPercentage > 1.0f)
-	{
-		fTimer = 0.0f;
-		std::swap(v3Start, v3End);
-	}
+	CheckSAT();
 
 	//Indicate the FPS
 	int nFPS = m_pSystem->GetFPS();
@@ -93,16 +87,61 @@ void AppClass::Update(void)
 	{
 		m_pMeshMngr->Print(std::to_string(list[n]) + " ", REYELLOW);
 	}
+
 	m_pMeshMngr->PrintLine(" ");
 	
 	m_pMeshMngr->Print("FPS:");
 	m_pMeshMngr->Print(std::to_string(nFPS), RERED);
+
+	m_pMeshMngr->PrintLine(" ");
+
+	m_pMeshMngr->Print("Dot:");
+	m_pMeshMngr->Print(std::to_string(glm::dot(glm::normalize(m_v3O1), glm::normalize(m_v3O2))), RERED);
 }
 
 void AppClass::Display(void)
 {
+	//Ok, here's how this fun stuff works
+	matrix4 planeMatrix = IDENTITY_M4;
+
+	//get steve and creeper centers, for the sake of using easier variables
+	vector3 creeperCenter = m_pBOMngr->GetBoundingObject("Creeper")->GetCenterGlobal();
+	vector3 steveCenter = m_pBOMngr->GetBoundingObject("Steve")->GetCenterGlobal();
+	//finding the global center point between both objects
+	vector3 centerPos = vector3((steveCenter.x + creeperCenter.x)/2, (steveCenter.y + creeperCenter.y) / 2, (steveCenter.z + creeperCenter.z) / 2);
+	//move the plane to the global center pos
+	planeMatrix = glm::translate(centerPos);
+	//find the vector between the center points of both objects, multiplied by steve's rotation
+	vector3 vectorBetween = vector3((steveCenter.x - creeperCenter.x), (steveCenter.y - creeperCenter.y), (steveCenter.z - creeperCenter.z)) * m_qArcBall;
+	//absolute value the vector, since we only care about positive numbers
+	vectorBetween = vector3(abs(vectorBetween.x), abs(vectorBetween.y), abs(vectorBetween.z));
+	quaternion q;
+	//if the vectorBetween's x is the largest, the plane is on the x axis
+	if (vectorBetween.x >= vectorBetween.y && vectorBetween.x >= vectorBetween.z)
+	{
+		q = glm::angleAxis(90.0f, vector3(0.0f, 1.0f, 0.0f));
+		m_pPlane->GeneratePlane(5.0f, RERED);
+	}
+	//if the vectorBetween's y is the largest, the plane is on the y axis
+	if (vectorBetween.y >= vectorBetween.x && vectorBetween.y > vectorBetween.z)
+	{
+		q = glm::angleAxis(90.0f, vector3(1.0f, 0.0f, 0.0f));
+		m_pPlane->GeneratePlane(5.0f, REGREEN);
+	}
+	//if the vectorBetween's z is the largest, the plane is on the z axis
+	if (vectorBetween.z > vectorBetween.x && vectorBetween.z > vectorBetween.y)
+	{
+		q = glm::angleAxis(90.0f, vector3(0.0f, 0.0f, 1.0f));
+		m_pPlane->GeneratePlane(5.0f, REBLUE);
+	}
+
+	//rotate the place by the rotation of steve
+	planeMatrix *= ToMatrix4(m_qArcBall * q);
+
 	//clear the screen
 	ClearScreen();
+
+	m_pPlane->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), planeMatrix);
 	//Render the grid based on the camera's mode:
 	m_pMeshMngr->AddGridToRenderListBasedOnCamera(m_pCameraMngr->GetCameraMode());
 	m_pMeshMngr->Render(); //renders the render list
